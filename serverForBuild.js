b0acdb8805402c003ca277902c78ffd5a5456ffd
david <davidfarley7171@gmail.com>
2019-07-21 18:40:59 -0700

    removed the get all

--- serverForBuild.js
+++ serverForBuild.js
@@ -33,9 +33,9 @@ app.get('/postgres', (req, response) => {
 });
 
 // send the user to index html page inspite of the url
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
